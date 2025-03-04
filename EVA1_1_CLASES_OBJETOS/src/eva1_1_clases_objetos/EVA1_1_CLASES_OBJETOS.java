59aeb4164c796d9eb6033dc2cf13595d7c27eba4
rubenhernandez-itch2 <81659471+rubenhernandez-itch2@users.noreply.github.com>
2025-03-03 18:58:47 -0600

    Add files via upload

--- EVA1_1_CLASES_OBJETOS/src/eva1_1_clases_objetos/EVA1_1_CLASES_OBJETOS.java
+++ EVA1_1_CLASES_OBJETOS/src/eva1_1_clases_objetos/EVA1_1_CLASES_OBJETOS.java
@@ -12,38 +12,31 @@ package eva1_1_clases_objetos;
 public class EVA1_1_CLASES_OBJETOS {
     
     public static void main(String[] args) {
-        //CREAR UN OBJETO
-        //INSTANCIAR
-        //Identificador para guardar la dirección en memoria
-        //variable de un tipo de dato abstracto
-        Persona perso1;//declaramos una variable de tipo persona
-        //asignamos memoria
-        perso1 = new Persona();//creamos un objeto de tipo persona
-        perso1.edad = 10;
+        //INSTANCIAR --> ASIGNAR MEMORIA AL CÓDIGO DE UNA CLASE
+        //PARA CREAR UN OBJETO
+        //Crear un identificador (variable)
+        Persona perso1; //Declaramos pero no creamos.
+        //Asignar memoria --> new
+        //REFERENCIA: Dirección en memoria.
+        perso1 = new Persona();
+        perso1.edad = 50;
         perso1.genero = "H";
         perso1.nombre = "Juan Perez";
-        perso1.imprimirDatos();
+        //perso1.imprimirDatos();
         System.out.println(perso1);
-        //JAVA:
-        //No accedemos a direcciones reales de memoria (RAM)
-        //A las direcciones de memoria (falsas) se les llama
-        //REFERENCIAS.
-        //APUNTADOR --> Variable que almacena una direccion (REAL) de memoria RAM
-        
-    }
-     
+    }    
 }
 
-class Persona{
+class Persona{//TIPO DE DATO ABSTRACTOS
     //DATOS - ESTADO - ATRIBUTOS
     String nombre;
     int edad;
     String genero;
-    //COMPORTAMIENTO
+    //COMPORTAMIENTO - METODOS
     void imprimirDatos(){
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Genero: " + genero);
+        System.out.println("Nombre = " + nombre);
+        System.out.println("Edad = " + edad);
+        System.out.println("Género = " + genero);
     }
 }
 
